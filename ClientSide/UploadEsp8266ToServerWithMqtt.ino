b2ded0073ed1beebfc4ead7ebe10a92ec95ce9be
Gil Leibovitz <leibovitzgil@gmail.com>
2017-09-16 13:02:21 +0300

    cleaned up the code

--- ClientSide/UploadEsp8266ToServerWithMqtt.ino
+++ ClientSide/UploadEsp8266ToServerWithMqtt.ino
@@ -1,27 +1,24 @@
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <DHT.h> 
+#include <DHT.h>
 #include <string>
 
-#define DHTPIN D5  
+#define DHTPIN D5
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
-float temperature, humidity, light;
+float temperature, humidity, soilHumidity;
 
 WiFiClient tsclient;
 
-#define MQTT_SERVER "192.168.14.69"
-//const char* ssid = "Bezeq-n_3FD6";
-//const char* password = "oritl4040";
-//const char* ssid = "AndroidAP";
-//const char* password = "xbjx1325";
-const char* ssid = "Orit";
-const char* password = "0507689766";
+#define MQTT_SERVER "192.168.43.49"
+const char* ssid = "AndroidAP";
+const char* password = "xbjx1325";
+const char id = '2';
 
 
 //mqtt setup
-char* lightTopic = "sensor";
+char* topic = "sensor";
 void callback(char* topic, byte* payload, unsigned int length); //forward declaration
 PubSubClient client(MQTT_SERVER, 1883, callback, tsclient);
 
@@ -29,7 +26,7 @@ PubSubClient client(MQTT_SERVER, 1883, callback, tsclient);
 
 void setup() {
   Serial.begin(115200);
-  dht.begin();  
+  dht.begin();
   delay(10);
 
   // We start by connecting to a WiFi network
@@ -38,37 +35,34 @@ void setup() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
-  WiFi.begin(ssid, password); //start wifi subsystem 
+
+  WiFi.begin(ssid, password); //start wifi subsystem
 
   reconnect(); //attempt to connect to the WIFI network and then connect to the MQTT server
- 
+
 }
 
 
 void loop() {
 
+  //sensors data
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  soilHumidity = analogRead(A0);
 
 
-  
-  //sensors data 
-  temperature = dht.readTemperature();  
-  humidity = dht.readHumidity(); 
-  light = analogRead(A0);
-
-//  Serial.print("connecting to ");
-//  Serial.println(MQTT_SERVER);
-  
-  Serial.print("Temperature Value is :");  
-  Serial.print(temperature);  
-  Serial.println("C");  
-  Serial.print("Humidity Value is :");  
-  Serial.print(humidity);  
+  Serial.print("Temperature Value is :");
+  Serial.print(temperature);
+  Serial.println("C");
+  Serial.print("Humidity Value is :");
+  Serial.print(humidity);
   Serial.println("%");
 
 
   //reconnect if connection is lost
-  if (!client.connected() && WiFi.status() == 3) {reconnect();}
+  if (!client.connected() && WiFi.status() == 3) {
+    reconnect();
+  }
 
   //maintain MQTT connection
   client.loop();
@@ -83,17 +77,17 @@ void loop() {
   url += "\"temperature\":";
   url += temperature;
   url += ",";
-  url += "\"light\":";
-  url += light;
+  url += "\"soilHumidity\":";
+  url += soilHumidity;
+  url += ",";
+  url += "\"id\":";
+  url += id;
   url += "}";
 
   const char * urlc = url.c_str(); //converting string to char*
 
   //publishing the data of the sensors
-  client.publish(lightTopic, urlc);
-  
-//  Serial.print("Requesting URL: ");
-//  Serial.println(url);
+  client.publish(topic, urlc);
   delay(20000);
 }
 
@@ -101,29 +95,18 @@ void loop() {
 void reconnect() {
 
   //attempt to connect to the wifi if connection is lost
-  if(WiFi.status() != WL_CONNECTED){
-    //debug printing
-//    Serial.print("Connecting to ");
-//    Serial.println(ssid);
+  if (WiFi.status() != WL_CONNECTED) {
 
     //loop while we wait for connection
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
-//      Serial.print(".");
     }
-
-    //print out some more debug once connected
-//    Serial.println("");
-//    Serial.println("WiFi connected");  
-//    Serial.println("IP address: ");
-//    Serial.println(WiFi.localIP());
   }
 
   //make sure we are connected to WIFI before attemping to reconnect to MQTT
-  if(WiFi.status() == WL_CONNECTED){
-  // Loop until we're reconnected to the MQTT server
+  if (WiFi.status() == WL_CONNECTED) {
+    // Loop until we're reconnected to the MQTT server
     while (!client.connected()) {
-//      Serial.print("Attempting MQTT connection...");
 
       // Generate client name based on MAC address and last 8 bits of microsecond counter
       String clientName;
@@ -135,53 +118,35 @@ void reconnect() {
       //if connected, subscribe to the topic(s) we want to be notified about
       if (client.connect((char*) clientName.c_str())) {
         Serial.print("\tMTQQ Connected");
-        client.subscribe(lightTopic);
+        client.subscribe(topic);
       }
 
       //otherwise print failed for debugging
-      else{Serial.println("\tFailed."); abort();}
+      else {
+        Serial.println("\tFailed.");
+        abort();
+      }
     }
   }
 }
 
 //generate unique name from MAC addr
-String macToStr(const uint8_t* mac){
+String macToStr(const uint8_t* mac) {
 
   String result;
 
   for (int i = 0; i < 6; ++i) {
     result += String(mac[i], 16);
 
-    if (i < 5){
+    if (i < 5) {
       result += ':';
     }
   }
-
   return result;
 }
 
+//i don't rellay need the callback method here, it's empty just to satisfy the mqtt signature
 void callback(char* topic, byte* payload, unsigned int length) {
-
-  //convert topic to string to make it easier to work with
-  String topicStr = topic; 
-
-  //Print out some debugging info
-  Serial.println("Callback update.");
-  Serial.print("Topic: ");
-  Serial.println(topicStr);
-
-  //turn the light on if the payload is '1' and publish to the MQTT server a confirmation message
-  if(payload[0] == '1'){
-    digitalWrite(LED_BUILTIN, HIGH);
-    client.publish("sensor", "Light On");
-
-  }
-
-  //turn the light off if the payload is '0' and publish to the MQTT server a confirmation message
-  else if (payload[0] == '0'){
-    digitalWrite(LED_BUILTIN, LOW);
-    client.publish("sensor", "Light Off");
-  }
 
 }
 
